93ddc6e83fe32f393828c316759b0aba694acdb4
Tim Cook <tim@mlhim.org>
2015-04-23 10:39:09 -0300

    Added migration.

---
+++ CMGenerator/s3model/cmgen/exceptions.py
@@ -0,0 +1,19 @@
+#application specific exceptions
+
+class MLHIMDatatypeError(Exception):
+      pass
+
+class PublishingError(Exception):
+      pass
+
+class AnnotationsError(Exception):
+      pass
+
+class ModellingError(Exception):
+      pass
+
+class GenerationError(Exception):
+      pass
+
+class CodesImportError(Exception):
+      pass
